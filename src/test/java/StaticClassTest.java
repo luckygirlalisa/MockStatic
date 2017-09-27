db75739c3016b8fda436db7d49927d49cff34f14
Zhanfei Xiao <zfxiao@thoughtworks.com>
2017-09-28 00:06:40 +0800

    Verify static method called times.

--- src/test/java/StaticClassTest.java
+++ src/test/java/StaticClassTest.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -12,10 +13,21 @@ public class StaticClassTest {
         PowerMockito.mockStatic(StaticDependency.class);
 
         SampleClass sampleClass = new SampleClass();
-
         sampleClass.doSomething("test");
 
-        PowerMockito.verifyStatic();
+        PowerMockito.verifyStatic(StaticDependency.class);
+        StaticDependency.staticMethod("test");
+    }
+
+    @Test
+    public void shouldVerifyNumbersOfCalls() throws Exception {
+        PowerMockito.mockStatic(StaticDependency.class);
+
+        SampleClass sampleClass = new SampleClass();
+        sampleClass.doSomething("test");
+
+
+        PowerMockito.verifyStatic(StaticDependency.class, Mockito.times(1));
         StaticDependency.staticMethod("test");
     }
 }
