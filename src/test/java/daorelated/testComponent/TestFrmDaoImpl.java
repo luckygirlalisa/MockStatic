a99828438ea6ee552fba87e1f360c2713395ec7c
zfxiao <luckygirlalisa@gmail.com>
2017-11-20 14:51:56 +0800

    Add sample sonar-project.properties file.

--- src/test/java/daorelated/testComponent/TestFrmDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package daorelated.testComponent;
-
-import daorelated.DaoInterface;
-
-public class TestFrmDaoImpl implements DaoInterface {
-    public String doSomething() {
-        return null;
-    }
-}
